e9ec549881b8d8c7b8e3666ae14d986bab86abec
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-06-01 14:44:14 +0400

    moved to docker format

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uzt4z0#uof4s0@!*09zpvd)774s=u%+$6mz+p!2#za@+ty)*_h'
+SECRET_KEY = config("SECRET_KEY", default="test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    cast=lambda v: [s.strip() for s in v.split(",")],
+    default="*",
+)
 
 
 # Application definition
@@ -135,17 +140,15 @@ LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
 # static and media configs
-if not DEBUG:
-    STATIC_ROOT = "/static/"
-    MEDIA_ROOT = "/media/"
-    
-else:
-    STATIC_ROOT = BASE_DIR / "static"
-    MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
-    STATICFILES_DIRS = [
-        (BASE_DIR / "staticfiles"),
-    ]
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
