23a8a8fa356d6666d56cbe480c83677fd9f1ca84
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-09-19 09:39:48 +0400

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -281,4 +281,6 @@ if SHOW_DEBUGGER_TOOLBAR:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
-    
+
+# disable checking for maximum number of fields
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
